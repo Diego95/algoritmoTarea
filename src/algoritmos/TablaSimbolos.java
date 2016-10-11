9772dd5aa9ef4232bf73c30c30a4b66c01eddc0d
Ariel <Ariel@Ariel-Bass>
2016-10-11 09:59:14 -0300

    wea 2.2

--- src/algoritmos/TablaSimbolos.java
+++ src/algoritmos/TablaSimbolos.java
@@ -5,11 +5,137 @@
  */
 package algoritmos;
 
+import java.util.Date;
+
 /**
  *
  * @author Diego
  */
-public class TablaSimbolos implements STInterface<Comparable<Key>, Object>
+public class TablaSimbolos  implements STInterface<Date,Object>
 {
+    private Nodo root;
+    public TablaSimbolos()
+    {
+        
+    }
+    class Nodo
+    {
+        private Date date;
+        private String contenido;
+        private Nodo next; 
+        public Nodo(Date date, String contenido)
+        {
+            this.date = date;
+            this.contenido = contenido;
+            this.next= null;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getContenido() {
+            return contenido;
+        }
+
+        public void setContenido(String contenido) {
+            this.contenido = contenido;
+        }
+
+        public Nodo getNext() {
+            return next;
+        }
+
+        public void setNext(Nodo next) {
+            this.next = next;
+        }
+        
+    }
+
+    @Override
+    public void put(Date key, Object value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object get(Date key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Date key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean contains(Date key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Date min() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Date floor(Date key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Date ceiling(Date key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int rank(Date key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Date select(int k) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delteMin() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteMax() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int size(Date lo, Date hi) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterable<Date> keys(Date lo, Date hi) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterable<Date> keys() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
     
 }
